42149eb5945ba40f7e899877636207c38bb17ca6
Marcel Klehr <mklehr@gmx.net>
2012-07-01 16:04:26 +0200

    Add License & NODIST_PREFIX

--- cli.js
+++ cli.js
@@ -1,3 +1,28 @@
+/*!
+ * nodist
+ * A Node version manager for the windows folks out there.
+ * Copyright 2012 by Marcel Klehr <mklehr@gmx.net>
+ *
+ * (MIT LICENSE)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
 var version = process.argv[2]
   , nodist  = require('./nodist')
   , fs      = require('fs')
@@ -25,7 +50,8 @@ if(process.argv[2] == '--help') {
 
 
 var n = new nodist(
-  nodistPath+'/../../node.exe',
+  (process.env['NODIST_PREFIX']
+  ? process.env['NODIST_PREFIX'] : nodistPath+'/../../' )+'node.exe',
   'http://nodejs.org/dist',
   nodistPath+'/v'
 );
