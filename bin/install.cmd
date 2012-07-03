fba3b5f771239f7f2cca6a7daf1810bad505f475
Marcel Klehr <mklehr@gmx.net>
2012-07-03 09:49:43 +0200

    Fix install/uninstall

---
+++ bin/install.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+:: Set up nodist command
+copy "%~dp0\bin.cmd" "%npm_config_prefix%\nodist.cmd"
---
+++ bin/uninstall.cmd
@@ -0,0 +1,4 @@
+@echo off
+
+:: Remove nodist executable
+del "%npm_config_prefix%\nodist.cmd"
