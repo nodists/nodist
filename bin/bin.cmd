5544be5fa90c091279e5f5c560ce7c8e295e4d54
Marcel Klehr <mklehr@gmx.net>
2012-07-04 22:01:55 +0200

    Don't bundle node, anymore. People must have 0.8.x though!

--- bin/bin.cmd
+++ bin/bin.cmd
@@ -1,2 +1,2 @@
 @echo off
-"%~dp0\.\node_modules\nodist\bin\node.exe" "%~dp0\.\node_modules\nodist\cli" %*
+"%~dp0\.nodist\node.exe" "%~dp0\.\node_modules\nodist\cli" %*
--- bin/install.cmd
+++ bin/install.cmd
@@ -2,3 +2,7 @@
 
 :: Set up nodist command
 copy "%~dp0\bin.cmd" "%npm_config_prefix%\nodist.cmd"
+
+:: copy my node.exe to .nodist\
+mkdir "%~dp0\..\..\..\.nodist"
+if not exist "%~dp0\..\..\..\.nodist\node.exe" copy "%~dp0\..\..\..\node.exe" "%~dp0\..\..\..\.nodist\node.exe"
