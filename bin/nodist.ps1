c2efd0d68146dcb802f569ab4099c33e55b15126
Marcel Klehr <mklehr@gmx.net>
2014-03-15 11:55:33 +0100

    Fix `nodist -v` in powershell
    Fixes #49

--- bin/nodist.ps1
+++ bin/nodist.ps1
@@ -1,7 +1,10 @@
-param($cmd,$ver)
+param($cmd,$ver,[Switch]$v)
 if ($cmd -eq "use") {
   $env:path = (nodist.cmd path $ver)+";"+$env:path
   nodist.cmd + $ver
+}
+elseif ($v){
+  nodist.cmd -v
 }
 else {
   nodist.cmd $cmd $ver ([string]$args)
