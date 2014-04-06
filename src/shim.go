1d80b224e56ba435d5af52a9e22d866d00065845
Marcel Klehr <mklehr@gmx.net>
2014-04-06 12:45:57 +0200

    Use a binary shim

---
+++ src/shim.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "os/exec"
+  "syscall"
+  "io/ioutil"
+  "strings"
+)
+
+func main() {
+  var (
+    version string = ""
+    x64 = false
+    path string
+    nodebin string
+  )
+  
+  // Determine version
+  
+  if v := os.Getenv("NODE_VERSION"); v != "" {
+    version = v
+  } else
+  if v = os.Getenv("NODIST_VERSION"); v != "" {
+    version = v
+  } else 
+  if v, err := ioutil.ReadFile("./.node-version"); err == nil {
+    version = string(v)
+  } else
+  if v, err := ioutil.ReadFile(os.Getenv("NODIST_PREFIX")+"/.node-version"); err == nil {
+    version = string(v)
+  }
+  
+  
+  // Determine architecture
+  
+  // XXX: coud also use this: http://msdn.microsoft.com/en-us/library/windows/desktop/ms724958%28v=vs.85%29.aspx
+  if arch := os.Getenv("PROCESSOR_ARCHITECTURE"); arch != "" {
+    x64 = (arch == "x64")
+  }
+  if wantX64 := os.Getenv("NODIST_X64"); wantX64 != "" {
+    x64 = (wantX64 == "1")
+  }
+  
+  
+  // Set up binary path
+  
+  if x64 {
+    path = os.Getenv("NODIST_PREFIX")+"/v-x64"
+  } else {
+    path = os.Getenv("NODIST_PREFIX")+"/v"
+  }
+  
+  version = strings.Trim(version, " \r\n")
+  
+  if version != "" {
+    nodebin = path+"/"+version+"/node.exe"
+  }else {
+    nodebin = "node.exe"
+  }
+  
+  cmd := exec.Command(nodebin, os.Args[1:]...)
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os.Stderr
+  cmd.Stdin = os.Stdin
+  err := cmd.Run()
+  
+  if err != nil {
+    exitError, isExitError := err.(*(exec.ExitError))
+    if isExitError {
+      // You know it. Black Magic...
+      os.Exit(exitError.Sys().(syscall.WaitStatus).ExitStatus())
+    } else {
+      fmt.Println("Sorry, there's a problem with nodist.")
+      fmt.Println("Error: ", err)
+      os.Exit(42)
+    }
+  }
+}
