31c2e8d053210045a996271742dea957010f8b3c
Marcel Klehr <mklehr@gmx.net>
2015-07-13 21:45:56 +0200

    Support iojs through iojsvX.X.X version specs
    fixes #72

--- cli.js
+++ cli.js
@@ -56,6 +56,7 @@ process.title = 'nodist';
 if(!process.env['NODIST_PREFIX']) abort('Please set the path to the nodist directory in the NODIST_PREFIX environment variable.')
 
 var distUrl = 'http://nodejs.org/dist'
+var iojsDistUrl = 'https://iojs.org/dist/'
 var nodistPrefix = process.env['NODIST_PREFIX'].replace(/"/g, '')
 var proxy = (process.env.HTTP_PROXY || process.env.http_proxy || process.env.HTTPS_PROXY || process.env.https_proxy || "");
 var wantX64 = process.env['NODIST_X64']!=null? process.env['NODIST_X64']==1 : (process.arch=='x64'); // if the env var is set, use its value, other wise use process.arch
@@ -64,6 +65,7 @@ var envVersion = process.env['NODIST_VERSION']? process.env['NODIST_VERSION'].re
 // Create a nodist instance
 var n = new nodist(
   distUrl
+, iojsDistUrl
 , nodistPrefix
 , proxy.replace("https://", "http://") //replace https for http, nodejs.org/dist doesnt support https 
 , wantX64
