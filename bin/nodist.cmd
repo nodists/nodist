77e3add7232a3756f08270ba68de3ff1651f15d3
Marcel Klehr <mklehr@gmx.net>
2016-04-24 20:48:58 +0200

    Remove selfupdate command

--- bin/nodist.cmd
+++ bin/nodist.cmd
@@ -11,30 +11,6 @@ goto next1
   :: goto end
   GOTO end
 
-:next1
-:: hook `nodist update`
-if "%1"=="update" goto selfupdate
-if "%1"=="selfupdate" goto selfupdate
-goto next2
-
-:selfupdate
-  echo Installing latest stable version...
-  cmd /C nodist stable
-
-  cmd /C npm config set prefix "%NODIST_PREFIX%\bin"
-
-  echo Update dependencies...
-  pushd .
-  cd /D "%~dp0"
-  cd ..
-  cmd /C npm update
-  popd
-
-  
-  ::goto end
-   GOTO end
-
-:next2
 :main
 "%~dp0..\node.exe" "%~dp0..\cli" %*
 goto end
