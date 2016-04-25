2b1017bb31fa21cd8aaff8fbefc64f7ad0059fd3
Marcel Klehr <mklehr@gmx.net>
2016-04-25 17:01:22 +0200

    npm shim + global/local/env switching
    - Introduced .npm-version and NODIST_NPM_VERSION
    - Renamed NODIST_VERSION to NODIST_NODE_VERSION
    - added npm shim
    - Factored out common function into nodist.go
    - added subcommands for npm
    - added new functionality to lib/npm.js
    - adjust build script

--- bin/nodist.ps1
+++ bin/nodist.ps1
@@ -1,8 +1,13 @@
 param($cmd,$ver,[Switch]$v)
 if ($cmd -eq "use" -or $cmd -eq "env") {
   nodist.cmd add $ver
-  $env:NODIST_VERSION = $ver
+  $env:NODIST_NODE_VERSION = $ver
   echo $ver
+}
+elseif ($cmd -eq "npm" -and $ver -eq "env") {
+  nodist.cmd npm add $args[0]
+  $env:NODIST_NPM_VERSION = $args[0]
+  echo $args[0]
 }
 elseif ($v){
   nodist.cmd -v
